47ae7e6a5eaef6cd7227cac77b777a8e1a763aa5
xkimopro <kimonaspro99@gmail.com>
2022-02-03 01:00:13 +0200

    Added necessary functions and basic REPL ( Now make it more user friendly and add help)

---
+++ game.py
@@ -0,0 +1,105 @@
+
+import string, random, functools
+from collections import OrderedDict,Counter
+
+
+def isHeterogram(word):
+    letter_counter = Counter(list(word))
+    for cnt in letter_counter.values(): 
+        if cnt > 1: return False
+    return True    
+
+
+def updateScoresDecorator(func):
+        @functools.wraps(func)
+        def wrapper_decorator(*args, **kwargs):
+            value = func(*args, **kwargs)
+            args[0].updateScores() 
+            return value
+        return wrapper_decorator
+
+
+class Game:
+
+
+    @updateScoresDecorator
+    def __init__(self, five_letter_words):
+        self.freq = {}
+        self.uppercase_letters = string.ascii_uppercase
+        self.removed_letters = []
+        self.correct_letters_correct_positions = []
+        self.correct_letters_wrong_positions = []
+        self.five_letter_words = five_letter_words
+        self.five_letter_words_scores = {}
+    
+
+    @updateScoresDecorator
+    def removeLetter(self, letter):
+        self.removed_letters.append(letter)
+        new_five_letter_words = list(filter(lambda w: letter not in list(w), self.five_letter_words))
+        self.five_letter_words = new_five_letter_words
+
+    @updateScoresDecorator
+    def correctLetterCorrectPosition(self, letter, pos):
+        letter_pos_tuple = (letter,pos)
+        self.correct_letters_correct_positions.append(letter_pos_tuple)
+        new_five_letter_words = list(filter(lambda w: list(w)[pos] == letter, self.five_letter_words))
+        self.five_letter_words = new_five_letter_words
+    
+    @updateScoresDecorator
+    def correctLetterWrongPosition(self, letter, pos):
+        letter_pos_tuple = (letter,pos)
+        self.correct_letters_wrong_positions.append(letter_pos_tuple)
+        new_five_letter_words = list(filter(lambda word: self.correctLetterWrongPositionChecker(letter, pos, word) , self.five_letter_words))
+        self.five_letter_words = new_five_letter_words
+
+    def correctLetterWrongPositionChecker(self, letter, pos, word): 
+        if list(word)[pos] == letter: return False
+        for l in list(word): 
+            if l == letter: return True
+
+    @updateScoresDecorator
+    def secondLetterNotInPosition(self,letter, pos):
+        new_five_letter_words = list(filter(lambda w: letter != list(w)[pos], self.five_letter_words))
+        self.five_letter_words = new_five_letter_words
+
+    def wordScore(self, word ):
+        score = 0
+        for letter in list(word): score += self.freq[letter]
+        return score
+
+
+
+    def getLetterFrequencyFiveLetterWords(self,):
+        uppercase_letters_count = {u : 0 for u in self.uppercase_letters}
+        for word in self.five_letter_words:
+            letters = list(word)
+            for letter in letters: uppercase_letters_count[letter] += 1
+        self.freq = OrderedDict(uppercase_letters_count)
+
+    
+    def updateScores(self, ):
+        self.getLetterFrequencyFiveLetterWords()
+        five_letter_words_scores = {w : self.wordScore(w) for w in self.five_letter_words}
+        sorted_tuples = sorted(five_letter_words_scores.items(), key=lambda item: item[1], reverse=True)
+        self.five_letter_words_scores = {k: v for k, v in sorted_tuples}
+
+    def sampleFiveLetterWords(self, n ):
+        random.shuffle(self.five_letter_words)
+        for i , word in enumerate(self.five_letter_words):
+            print(word)
+            if i == n-1: break
+        print()
+
+    def sampleFiveLetterScores(self, n, heterogram):
+        times = 0
+        for word, score in self.five_letter_words_scores.items():
+            if n == times: break
+            if not isHeterogram(word) and heterogram: continue
+            print(word + " " + str(score))
+            times+=1
+        
+        print()
+
+    def testingFunction(self,):
+        print("Test")
---
+++ worlde_tool.py
@@ -0,0 +1,71 @@
+from os import stat
+from game import Game
+from functions import *
+
+
+dictionary_file = "words.txt"
+
+with open(dictionary_file) as df:
+    five_letter_words = df.read().split()
+    statistics = Game(five_letter_words)
+
+    while True:
+        input_line = input()
+        for command in input_line.split():
+            
+            if 'sample' in command:
+                statistics.sampleFiveLetterScores(20, heterogram=True)
+                continue
+            
+            char, pos = getCharPos(command)
+            
+
+
+            if 'Green' in command:
+                statistics.correctLetterCorrectPosition(char,pos)
+                
+            if 'Yellow' in command:
+                statistics.correctLetterWrongPosition(char,pos)
+
+            if 'Gray' in command:
+                if 'Gray2' in command:
+                    statistics.secondLetterNotInPosition(char,pos)
+                else:
+                    char = insideParentheses(command)
+                    statistics.removeLetter(char)
+
+           
+            if input_line[-1] == ';': statistics.sampleFiveLetterScores(20, heterogram=True)
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+    # statistics.removeLetter('H')
+    # statistics.correctLetterCorrectPosition('T', 4)
+    # statistics.correctLetterWrongPosition('I', 1)
+    
+    
+    # statistics.removeLetter('F')
+    # statistics.removeLetter('L')
+    # statistics.removeLetter('N')
+
+    # statistics.correctLetterCorrectPosition('I', 2)
+    # statistics.sampleFiveLetterWords(100)
+
+
+
+
